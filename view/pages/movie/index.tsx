be2cf27dd23e674584beafd428bbef86586af46c
shen4-4 <951630322@qq.com>
2017-08-14 10:37:02 +0800

    add movie page

--- view/pages/movie/index.tsx
+++ view/pages/movie/index.tsx
@@ -4,7 +4,7 @@ import { RouteComponentProps } from 'react-router-dom'
 import { Button, Modal, Input, message, Layout, Menu, Breadcrumb } from 'antd'
 import { CommonStore } from '../../store/common'
 
-import './index.css'
+import './index.scss'
 
 interface Props extends RouteComponentProps<any> {
   commonStore: CommonStore,
@@ -21,15 +21,69 @@ export class Movie extends React.Component<Props, {}> {
   render() {
     return (
       <div className='wrapper'>
-          <div className="block">A</div>
-          <div className="block">B</div>
-          <div className="block">C</div>
-          <div className="block">D</div>
-          <div className="block">E</div>
-          <div className="block">F</div>
-          <div className="block">D</div>
-          <div className="block">E</div>
-          <div className="block">F</div>
+          <div className="block">
+              <div className="intro">
+                  <h1><a href="http://douban.com">黑皮书</a></h1>
+                  <div className='text'>豆瓣评分</div>
+                  <div className='text'>我的评分</div>
+              </div>
+              <img className='heipishu'></img>
+          </div>
+          <div className="block">
+              <div className="intro">
+                  <h1><a href="http://douban.com">银河护卫队</a></h1>
+                  <div className='text'>豆瓣评分</div>
+                  <div className='text'>我的评分</div>
+              </div>
+              <img className='yinhe'></img>
+          </div>
+          <div className="block">
+              <div className="intro">
+                  <h1><a href="http://douban.com">小林家的龙女仆</a></h1>
+                  <div className='text'>豆瓣评分</div>
+                  <div className='text'>我的评分</div>
+              </div>
+              <img className='long'></img>
+          </div>
+          <div className="block">
+              <div className="intro">
+                  <h1><a href="http://douban.com">小林家的龙女仆</a></h1>
+                  <div className='text'>豆瓣评分</div>
+                  <div className='text'>我的评分</div>
+              </div>
+              <img className='long'></img>
+          </div>
+          <div className="block">
+              <div className="intro">
+                  <h1><a href="http://douban.com">黑皮书</a></h1>
+                  <div className='text'>豆瓣评分</div>
+                  <div className='text'>我的评分</div>
+              </div>
+              <img className='heipishu'></img>
+          </div>
+          <div className="block">
+              <div className="intro">
+                  <h1><a href="http://douban.com">银河护卫队</a></h1>
+                  <div className='text'>豆瓣评分</div>
+                  <div className='text'>我的评分</div>
+              </div>
+              <img className='yinhe'></img>
+          </div>
+          <div className="block">7</div>
+          <div className="block">8</div>
+          <div className="block">9</div>
+          <div className="block">10</div>
+          <div className="block">11</div>
+          <div className="block">12</div>
+          <div className="block">13</div>
+          <div className="block">14</div>
+          <div className="block">15</div>
+          <div className="block">16</div>
+          <div className="block">17</div>
+          <div className="block">18</div>
+          <div className="block">19</div>
+          <div className="block">20</div>
+          <div className="block">21</div>
       </div>
     )
   }
